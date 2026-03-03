6cec8639038072efc0c5d92aec52e3d565108521
mrbuddhu <i.am.mrbuddhu@gmail.com>
2026-03-04 01:54:15 +0530

    Update WhatsAppFloat component to improve visibility on medium and larger screens by changing the display property. The button is now hidden on smaller screens and displayed as a flex container on medium screens and above.

--- components/WhatsAppFloat.tsx
+++ components/WhatsAppFloat.tsx
@@ -7,7 +7,7 @@ const floatBtn =
 
 export function WhatsAppFloat() {
   return (
-    <div className="fixed bottom-20 right-4 z-40 flex flex-col gap-3 md:bottom-8">
+    <div className="fixed bottom-20 right-4 z-40 hidden flex-col gap-3 md:bottom-8 md:flex">
       <a
         href={PHONE_URL}
         className={`${floatBtn} btn-float-3d-gold bg-gold hover:bg-gold-light`}
